2823b467d341893d8367f75551b8c2ebd379dd75
josyvine <64621103+josyvine@users.noreply.github.com>
2026-07-03 23:51:35 +0530

    Update CentralConfig.java

--- app/src/main/java/com/safevoice/app/utils/CentralConfig.java
+++ app/src/main/java/com/safevoice/app/utils/CentralConfig.java
@@ -12,28 +12,28 @@ public final class CentralConfig {
     }
 
     /**
-     * The Google Web Client ID from your central project's OAuth 2.0 client IDs.
+     * The Google Web Client ID from your central project's OAuth 2.0 client IDs (Client Type 3).
      * This is required to request the ID token during the Google Sign-In handshake.
      */
-    public static final String WEB_CLIENT_ID = "YOUR_CENTRAL_WEB_CLIENT_ID_HERE.apps.googleusercontent.com";
+    public static final String WEB_CLIENT_ID = "1013718891067-ogca31bvt1f2rvcf0jed41gccienmdg8.apps.googleusercontent.com";
 
     /**
      * The API Key from your central Firebase project configuration.
      */
-    public static final String API_KEY = "YOUR_CENTRAL_API_KEY_HERE";
+    public static final String API_KEY = "AIzaSyBIQwGtpgZw-uVvJcFndrlCb6hllGMBbs0";
 
     /**
      * The Application ID (mobilesdk_app_id) for your Android app in your central Firebase project.
      */
-    public static final String APPLICATION_ID = "YOUR_CENTRAL_APPLICATION_ID_HERE";
+    public static final String APPLICATION_ID = "1:1013718891067:android:14fafe142776740fe57a1e";
 
     /**
      * The Project ID of your central developer Firebase project.
      */
-    public static final String PROJECT_ID = "YOUR_CENTRAL_PROJECT_ID_HERE";
+    public static final String PROJECT_ID = "safevoice-a09cf";
 
     /**
      * The Storage Bucket URL of your central developer Firebase project.
      */
-    public static final String STORAGE_BUCKET = "YOUR_CENTRAL_STORAGE_BUCKET_HERE";
+    public static final String STORAGE_BUCKET = "safevoice-a09cf.firebasestorage.app";
 }
